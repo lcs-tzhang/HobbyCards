4297677f1b35c1363a60e8ced94b7b1e6ab6b10b
Tiffany Zhang <tzhang@lcs.on.ca>
2024-12-14 14:44:27 -0500

    Initial Commit

---
+++ HobbyCards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HobbyCards
+//
+//  Created by xinyu zhang on 2024-12-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HobbyCards/HobbyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbyCardsApp.swift
+//  HobbyCards
+//
+//  Created by xinyu zhang on 2024-12-14.
+//
+
+import SwiftUI
+
+@main
+struct HobbyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

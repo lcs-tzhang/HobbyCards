14590584eab0ede52b3a8e3dd7b36be1299abb28
Tiffany Zhang <tzhang@lcs.on.ca>
2024-12-14 16:54:31 -0500

    finished coverView

---
+++ HobbyCards/Views/CoverView.swift
@@ -0,0 +1,54 @@
+//
+//  LandingView.swift
+//  HobbyCards
+//
+//  Created by xinyu zhang on 2024-12-14.
+//
+
+import SwiftUI
+
+struct CoverView: View {
+    var body: some View {
+        ZStack{
+            //Background
+            LinearGradient(
+                colors:[
+                    .gradientBlue,
+                    .gradientPurple,
+                    .gradientPink,
+                    .gradientPink,
+                    .gradientPurple,
+                    .gradientBlue
+                ],
+                startPoint: .top,
+                endPoint: .bottom
+                )
+            .ignoresSafeArea()
+            
+            // Foreground
+            
+            VStack(spacing :0){
+                Text("Tourist attractions Cards")
+                    .font(.custom("Bradley Hand",fixedSize: 64.0))
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.white)
+                    .rotationEffect(.degrees(-5.0))
+                    .padding(.bottom)
+                
+            Spacer()
+                    .frame(height:20)
+                
+                Image("TravelingPicture")
+                    .resizable()
+                    .scaledToFit()
+                    
+                
+                    
+            }
+            }
+        }
+}
+
+#Preview {
+    CoverView()
+}
--- HobbyCards/Views/LandingView.swift
+++ HobbyCards/Views/LandingView.swift
@@ -9,44 +9,8 @@ import SwiftUI
 
 struct LandingView: View {
     var body: some View {
-        ZStack{
-            //Background
-            LinearGradient(
-                colors:[
-                    .gradientBlue,
-                    .gradientPurple,
-                    .gradientPink,
-                    .gradientPink,
-                    .gradientPurple,
-                    .gradientBlue
-                ],
-                startPoint: .top,
-                endPoint: .bottom
-                )
-            .ignoresSafeArea()
-            
-            // Foreground
-            
-            VStack(spacing :0){
-                Text("Tourist attractions Cards")
-                    .font(.custom("Bradley Hand",fixedSize: 64.0))
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(.white)
-                    .rotationEffect(.degrees(-5.0))
-                    .padding(.bottom)
-                
-            Spacer()
-                    .frame(height:20)
-                
-                Image("TravelingPicture")
-                    .resizable()
-                    .scaledToFit()
-                    
-                
-                    
-            }
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
 }
 
 #Preview {

eb2746dbda91e2df4c9acaf9510717e32da8b43b
Tiffany Zhang <tzhang@lcs.on.ca>
2024-12-14 22:14:43 -0500

    finish  place

--- HobbyCards/Views/PlaceTabView.swift
+++ HobbyCards/Views/PlaceTabView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct PlaceView: View {
+struct PlaceTabView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    PlaceView()
+    PlaceTabView()
 }
